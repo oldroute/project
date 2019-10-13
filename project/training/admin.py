deaddb9b49aea39d89037bcd0f81a9f4ccf21b1f
azat.zakirov <oldroutebox@gmail.com>
2019-10-13 15:22:59 +0500

    topc delete redirecs

--- project/training/admin.py
+++ project/training/admin.py
@@ -16,8 +16,11 @@ from django.urls import reverse
 from django.utils.encoding import force_text
 from django.utils.html import format_html
 from django.utils.http import urlquote
-from django.contrib.admin.utils import quote
 from django.utils.translation import ugettext as _
+from django.contrib.admin.exceptions import DisallowedModelAdminToField
+from django.contrib.admin.utils import quote, unquote
+from django.db import router
+from django.contrib.admin.utils import get_deleted_objects
 
 
 IS_POPUP_VAR = '_popup'
@@ -167,6 +170,86 @@ class TopicAdmin(admin.ModelAdmin):
             'order_key': Topic.objects.filter(course=self._course).count()
         })
         return initial
+
+    def response_delete(self, request, obj_display, obj_id, course_id):
+
+        opts = self.model._meta
+
+        self.message_user(request, _('The %(name)s "%(obj)s" was deleted successfully.') % {
+            'name': force_text(opts.verbose_name),
+            'obj': force_text(obj_display),
+        }, messages.SUCCESS,)
+
+        if self.has_change_permission(request, None):
+            post_url = reverse('admin:training_course_change', args=[course_id], current_app=self.admin_site.name)
+            preserved_filters = self.get_preserved_filters(request)
+            post_url = add_preserved_filters({'preserved_filters': preserved_filters, 'opts': opts}, post_url)
+        else:
+            post_url = reverse('admin:index', current_app=self.admin_site.name)
+        return HttpResponseRedirect(post_url)
+
+    def _delete_view(self, request, object_id, extra_context):
+
+        opts = self.model._meta
+        app_label = opts.app_label
+
+        to_field = request.POST.get(TO_FIELD_VAR, request.GET.get(TO_FIELD_VAR))
+        if to_field and not self.to_field_allowed(request, to_field):
+            raise DisallowedModelAdminToField("The field %s cannot be referenced." % to_field)
+
+        obj = self.get_object(request, unquote(object_id), to_field)
+
+        if not self.has_delete_permission(request, obj):
+            raise PermissionDenied
+
+        if obj is None:
+            return self._get_obj_does_not_exist_redirect(request, opts, object_id)
+
+        using = router.db_for_write(self.model)
+
+        # Populate deleted_objects, a data structure of all related objects that
+        # will also be deleted.
+        (deleted_objects, model_count, perms_needed, protected) = get_deleted_objects(
+            [obj], opts, request.user, self.admin_site, using)
+
+        if request.POST and not protected:  # The user has confirmed the deletion.
+            if perms_needed:
+                raise PermissionDenied
+            obj_display = force_text(obj)
+            attr = str(to_field) if to_field else opts.pk.attname
+            course_id = obj.course.id
+            obj_id = obj.serializable_value(attr)
+            self.log_deletion(request, obj, obj_display)
+            self.delete_model(request, obj)
+
+            return self.response_delete(request, obj_display, obj_id, course_id)
+
+        object_name = force_text(opts.verbose_name)
+
+        if perms_needed or protected:
+            title = _("Cannot delete %(name)s") % {"name": object_name}
+        else:
+            title = _("Are you sure?")
+
+        context = dict(
+            self.admin_site.each_context(request),
+            title=title,
+            object_name=object_name,
+            object=obj,
+            deleted_objects=deleted_objects,
+            model_count=dict(model_count).items(),
+            perms_lacking=perms_needed,
+            protected=protected,
+            opts=opts,
+            app_label=app_label,
+            preserved_filters=self.get_preserved_filters(request),
+            is_popup=(IS_POPUP_VAR in request.POST or
+                      IS_POPUP_VAR in request.GET),
+            to_field=to_field,
+        )
+        context.update(extra_context or {})
+
+        return self.render_delete_form(request, context)
 
 
 @admin.register(Course)
