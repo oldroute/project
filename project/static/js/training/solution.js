48c80480e5742fc10a60b606ca4d5fe90e8162da
azat.zakirov <oldroutebox@gmail.com>
2019-11-16 20:22:28 +0500

    Стилизация решений пользователя

--- project/static/js/training/solution.js
+++ project/static/js/training/solution.js
@@ -18,12 +18,18 @@ var solutionPage = function(e){
 }
 
 var getUserSolutions = function(e){
-    console.log()
     $.get(e.target.userSolutionUrl, function(response){
         for (const [key, val] of Object.entries(response)) {
             var elem = document.querySelector('#' + key)
             elem && elem.classList.add('status__' + val)
         }
+        document.querySelectorAll('.js__topic-item').forEach(function(elem){
+            var taskitems = elem.querySelectorAll('.js__taskitem-item.status__3')
+            var taskitems_success = elem.querySelectorAll('.js__taskitem-item')
+            if(taskitems.length != 0 && taskitems.length == taskitems_success.length){
+                elem.querySelector('.js__topic-item-title').classList.add('status__3')
+            }
+        })
     })
 }
 
--- project/training/templatetags/training_tags.py
+++ project/training/templatetags/training_tags.py
@@ -6,8 +6,11 @@ register = template.Library()
 
 @register.filter
 def date_format(str_datetime):
-    val = datetime.strptime(str_datetime, '%Y-%m-%d %H:%M:%S.%f')
-    return val.strftime('%Y.%m.%d: %H:%M')
+    if str_datetime:
+        val = datetime.strptime(str_datetime, '%Y-%m-%d %H:%M:%S.%f')
+        return val.strftime('%Y.%m.%d: %H:%M')
+    else:
+        return '-'
 
 
 @register.inclusion_tag('training/parts/breadcrumbs.html', takes_context=True)
